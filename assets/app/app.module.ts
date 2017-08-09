1e548b3b9dafa5ff5b9ed8cf8bf09808293b52d0
Mike Z <minqi03@gmail.com>
2017-08-08 17:25:40 -0700

    message and auth module

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from "./app.component";
-import { MessageComponent } from "./messages/message.component";
-import { MessageListComponent } from "./messages/message-list.component";
-import { MessageInputComponent } from "./messages/message-input.component";
-import { MessagesComponent } from "./messages/messages.component";
 import { AuthenticationComponent } from "./auth/authentication.component";
 import { HeaderComponent } from "./header.component";
 
-import { SigninComponent } from "./auth/signin.component";
-import { SignupComponent } from "./auth/signup.component";
-import { LogoutComponent } from "./auth/logout.component";
+// import { SigninComponent } from "./auth/signin.component";
+// import { SignupComponent } from "./auth/signup.component";
+// import { LogoutComponent } from "./auth/logout.component";
 
 import { ErrorComponent } from "./errors/error.component";
 import { ErrorService } from "./errors/error.service";
@@ -22,22 +18,16 @@ import { AuthService } from "./auth/auth.service";
 
 import { routing } from "./app.routing";
 
+import { MessageModule } from "./messages/message.module";
+
 @NgModule({
     declarations: [
         AppComponent,
-        MessageComponent,
-        MessageListComponent,
-        MessageInputComponent,
-        MessagesComponent,
         AuthenticationComponent,
         HeaderComponent,
-        LogoutComponent,
-        SigninComponent,
-        SignupComponent,
-        LogoutComponent,
         ErrorComponent
     ],
-    imports: [BrowserModule, FormsModule, routing, ReactiveFormsModule, HttpModule],
+    imports: [BrowserModule, routing, HttpModule, MessageModule],
     providers: [AuthService, ErrorService],
     bootstrap: [AppComponent]
 })
